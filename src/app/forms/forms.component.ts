1a24a5f89cfd5d1f62f45bd77b43cbeef4ee3e76
Arief Shah <Arief.shah@gmail.com>
2020-11-30 23:23:43 +1100

    initial push

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -32,17 +32,19 @@ export class FormsComponent implements OnInit {
   ];
 
   countries = [
-    new Country('UY', 'Uruguay'),
-    new Country('US', 'United States'),
-    new Country('AR', 'Argentina')
-  ];
+     new Country('US', 'United States'),
+     ];
 
 
   validation_messages = {
     'fullname': [
       { type: 'required', message: 'Full name is required' }
     ],
-    'bio': [
+    'email': [
+      { type: 'required', message: 'Email is required' },
+      { type: 'pattern', message: 'Enter a valid email' }
+    ],
+    'body': [
       { type: 'maxlength', message: 'Bio cannot be more than 256 characters long' },
     ],
     'gender': [
@@ -56,32 +58,6 @@ export class FormsComponent implements OnInit {
       { type: 'validCountryPhone', message: 'Phone incorrect for the country selected' }
     ]
   };
-
-  account_validation_messages = {
-    'username': [
-      { type: 'required', message: 'Username is required' },
-      { type: 'minlength', message: 'Username must be at least 5 characters long' },
-      { type: 'maxlength', message: 'Username cannot be more than 25 characters long' },
-      { type: 'pattern', message: 'Your username must contain only numbers and letters' },
-      { type: 'validUsername', message: 'Your username has already been taken' }
-    ],
-    'email': [
-      { type: 'required', message: 'Email is required' },
-      { type: 'pattern', message: 'Enter a valid email' }
-    ],
-    'confirm_password': [
-      { type: 'required', message: 'Confirm password is required' },
-      { type: 'areEqual', message: 'Password mismatch' }
-    ],
-    'password': [
-      { type: 'required', message: 'Password is required' },
-      { type: 'minlength', message: 'Password must be at least 5 characters long' },
-      { type: 'pattern', message: 'Your password must contain at least one uppercase, one lowercase, and one number' }
-    ],
-    'terms': [
-      { type: 'pattern', message: 'You must accept terms and conditions' }
-    ]
-  }
 
   constructor(private fb: FormBuilder) { }
 
@@ -119,30 +95,18 @@ export class FormsComponent implements OnInit {
 
     // user details form validations
     this.userDetailsForm = this.fb.group({
-      fullname: ['Homero Simpson', Validators.required ],
-      bio: ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s", Validators.maxLength(256)],
+      fullname: ['', Validators.required ],
+      body: ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s", Validators.maxLength(256)],
       birthday: ['', Validators.required],
       gender: new FormControl(this.genders[0], Validators.required),
-      country_phone: this.country_phone_group
-    });
-
-
-    // user links form validations
-    this.accountDetailsForm = this.fb.group({
-      username: new FormControl('', Validators.compose([
-       UsernameValidator.validUsername,
-       Validators.maxLength(25),
-       Validators.minLength(5),
-       Validators.pattern('^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$'),
-       Validators.required
-      ])),
+      country_phone: this.country_phone_group,
       email: new FormControl('', Validators.compose([
         Validators.required,
         Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
       ])),
-      matching_passwords: this.matching_passwords_group,
-      terms: new FormControl(false, Validators.pattern('true'))
-    })
+    });
+
+
 
   }
 
